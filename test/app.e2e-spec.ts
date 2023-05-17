6c1d69725dcfe7263e1f987aeb08f67fdee43b6f
firerum <ademuyiwaadewuyi@gmail.com>
2023-05-17 20:05:32 +0100

    e2e WIP

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,7 +5,8 @@ import { AppModule } from './../src/app.module';
 import { PgService } from 'src/pg/pg.service';
 
 let token = '';
-let id = '';
+let userId = '';
+let eventId = '';
 
 afterAll(async () => {
   const pg = new PgService();
@@ -14,7 +15,7 @@ afterAll(async () => {
     'kaindoe@example.com',
   ]);
 
-  await pg.pool.query('DELETE FROM event_entity WHERE user_id = $1', [id]);
+  await pg.pool.query('DELETE FROM event_entity WHERE user_id = $1', [userId]);
 
   pg.pool.end();
 });
@@ -39,7 +40,7 @@ describe('AppController (e2e)', () => {
     server = request(app.getHttpServer());
   });
 
-  describe('User Controller', () => {
+  describe('Auth controller', () => {
     it('should sign up new user (POST)', async () => {
       const response = await server.post('/api/auth/signup').send({
         first_name: 'kain',
@@ -47,8 +48,6 @@ describe('AppController (e2e)', () => {
         email: 'kaindoe@example.com',
         password: '123456',
       });
-      token = response.body.token;
-      id = response.body.id;
 
       expect(response.status).toBe(201);
       expect(response.body).toHaveProperty('id');
@@ -56,6 +55,92 @@ describe('AppController (e2e)', () => {
       expect(response.body.email).toBe('kaindoe@example.com');
     });
 
+    it('should login new user (POST)', async () => {
+      const response = await server.post('/api/auth/signin').send({
+        email: 'kaindoe@example.com',
+        password: '123456',
+      });
+
+      token = response.body.token;
+      userId = response.body.id;
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('id');
+      expect(response.body.first_name).toBe('kain');
+      expect(response.body.email).toBe('kaindoe@example.com');
+    });
+  });
+
+  // Events Tests
+  describe('Event Controller', () => {
+    it('should create new event (POST)', async () => {
+      const response = await server
+        .post('/api/events')
+        .send({
+          name: 'Test Event',
+          category: 'wedding',
+          venue: 'ilasamoja, Lagos',
+          date_of_event: '2',
+          description: 'this is a test event.',
+          visibility: 'private',
+          user_id: userId,
+        })
+        .set('Authorization', `Bearer ${token}`);
+      eventId = response.body.id;
+
+      expect(response.status).toBe(201);
+      expect(response.body).toHaveProperty('id');
+      expect(response.body.name).toBe('test event');
+      expect(response.body.description).toBe('this is a test event.');
+    });
+
+    it('should get all events (GET)', async () => {
+      const response = await server
+        .get('/api/events')
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toBeInstanceOf(Array);
+    });
+
+    it('should get a single event (GET)', async () => {
+      // Assuming an event with ID 1 exists
+      const response = await server
+        .get(`/api/events/${eventId}`)
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('id');
+      expect(response.body).toHaveProperty('name');
+      expect(response.body).toHaveProperty('description');
+    });
+
+    it('should update an event (PUT)', async () => {
+      // Assuming an event with ID 1 exists
+      const response = await server
+        .put(`/api/events/${eventId}`)
+        .send({
+          name: 'Updated Event',
+        })
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('id');
+      expect(response.body.name).toBe('updated event');
+      expect(response.body).toHaveProperty('description');
+    });
+
+    it('should delete event (DELETE)', async () => {
+      // Assuming an event with ID 1 exists
+      const response = await server
+        .delete(`/api/events/${eventId}`)
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(204);
+    });
+  });
+
+  describe('User Controller', () => {
     it('should get all users (GET)', async () => {
       const response = await server
         .get('/api/users')
@@ -68,7 +153,7 @@ describe('AppController (e2e)', () => {
     it('should get single user (GET)', async () => {
       // Assuming a user with ID 1 exists
       const response = await server
-        .get(`/api/users/${id}`)
+        .get(`/api/users/${userId}`)
         .set('Authorization', `Bearer ${token}`);
 
       expect(response.status).toBe(200);
@@ -80,7 +165,7 @@ describe('AppController (e2e)', () => {
     it('should update the user (PUT)', async () => {
       // Assuming a user with ID 1 exists
       const response = await server
-        .put(`/api/users/${id}`)
+        .put(`/api/users/${userId}`)
         .send({
           first_name: 'Updated Name',
         })
@@ -94,77 +179,10 @@ describe('AppController (e2e)', () => {
 
     it('should delete the user (DELETE)', async () => {
       const response = await server
-        .delete(`/api/users/${id}`)
+        .delete(`/api/users/${userId}`)
         .set('Authorization', `Bearer ${token}`);
 
       expect(response.status).toBe(200);
-    });
-  });
-
-  // Events Tests
-  describe('Event Controller', () => {
-    it('should create new event (POST)', async () => {
-      const response = await server
-        .post('/api/events')
-        .send({
-          name: 'Test Event',
-          category: 'wedding',
-          venue: 'ilasamoja, Lagos',
-          date_of_event: '2023-05-08 17:36:48.031+01',
-          description: 'This is a test event.',
-          visibility: 'private',
-          user_id: id,
-        })
-        .set('Authorization', `Bearer ${token}`);
-      console.log(response.body);
-      console.log(id);
-
-      expect(response.status).toBe(201);
-      expect(response.body).toHaveProperty('id');
-      expect(response.body.name).toBe('Test Event');
-      expect(response.body.description).toBe('This is a test event.');
-    });
-
-    it('should get all events (GET)', async () => {
-      const response = await server
-        .get('/api/events')
-        .set('Authorization', `Bearer ${token}`);
-
-      expect(response.status).toBe(200);
-      expect(response.body).toBeInstanceOf(Array);
-    });
-
-    it('/events/:id (GET)', async () => {
-      // Assuming an event with ID 1 exists
-      const response = await server.get('/events/1');
-
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('id');
-      expect(response.body).toHaveProperty('title');
-      expect(response.body).toHaveProperty('description');
-    });
-
-    it('/events/:id (PUT)', async () => {
-      // Assuming an event with ID 1 exists
-      const response = await server.put('/events/1').send({
-        title: 'Updated Event',
-      });
-
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('id');
-      expect(response.body.title).toBe('Updated Event');
-      expect(response.body).toHaveProperty('description');
-    });
-
-    it('/events/:id (DELETE)', async () => {
-      // Assuming an event with ID 1 exists
-      const response = await server.delete('/events/1');
-
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty(
-        'message',
-        'Event deleted successfully',
-      );
     });
   });
 });
